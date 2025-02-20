d736f00ad539835e17a62ca7bcc177023a4ad9a1
Juan Hoyos <juanjosehoyos1206@gmail.com>
2025-02-20 13:21:05 -0500

    Mi primera linea de codigo

---
+++ inicio.py
@@ -0,0 +1,8 @@
+print("hola mundo")
+
+numero = int(input("ingrese un numero entre 1 y 10: "))
+
+if numero in (2, 4, 6, 8, 10):
+    print("su numero es par")
+elif numero in (1, 3, 5, 7, 9):
+    print("su numero es impar")
